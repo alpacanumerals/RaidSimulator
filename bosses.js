b25aef8ecad3444491f971a6fb5722e3b1afb04c
fusipon <writinginxanadu@gmail.com>
2021-04-26 22:33:16 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ bosses.js
@@ -0,0 +1,23 @@
+const deepCrystal = {
+  name: 'Lv50 Deep Crystal',
+  image: 'deepcrystal',
+  maxHp: 500000,
+  moveInterval: 0,
+  mechanics: [circleAoe, crossAoe]
+}
+
+const darkVermilion = {
+  name: 'Lv50 Dark Vermilion',
+  image: 'darkvermilion',
+  maxHp: 750000,
+  moveInterval: 5000,
+  mechanics: []
+}
+
+const forgivenCuteness = {
+  name: 'Lv50 Forgiven Cuteness',
+  image: 'forgivenCuteness',
+  maxHp: 1000000,
+  moveInterval: 5000,
+  mechanics: []
+}
